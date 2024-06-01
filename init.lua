c47635ea2afe4ee1828bf06984849f5385e50386
Sinisa Nimcevic <sinisa.nimcevic@yettel.rs>
2024-06-01 13:32:13 +0200

    add typescriptreact

--- init.lua
+++ init.lua
@@ -245,6 +245,38 @@ require('lazy').setup({
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'tpope/vim-fugitive',
   'tpope/vim-surround',
+  'nvim-neotest/nvim-nio',
+  {
+    'rcarriga/nvim-dap-ui',
+    event = 'VeryLazy',
+    dependencies = 'mfussenegger/nvim-dap',
+    config = function()
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+      dapui.setup()
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close()
+      end
+    end,
+  },
+  {
+    'jay-babu/mason-nvim-dap.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'williamboman/mason.nvim',
+      'mfussenegger/nvim-dap',
+    },
+    opts = {
+      handlers = {},
+    },
+  },
+  'mfussenegger/nvim-dap',
   {
     'theprimeagen/harpoon',
     event = 'VimEnter',
@@ -711,6 +743,8 @@ require('lazy').setup({
         -- is found.
         json = { { 'prettierd', 'prettier' } },
         javascript = { { 'prettierd', 'prettier' } },
+        typescript = { { 'prettierd', 'prettier' } },
+        typescriptreact = { { 'prettierd', 'prettier' } },
         -- Use the "_" filetype to run formatters on filetypes that don't
         -- have other formatters configured.
         ['_'] = { 'trim_whitespace' },
